11b41e7f13552639372b0f0c880a81d9a6d3d984
leonardofaria00 <leonardofaria00@gmail.com>
2020-05-11 13:55:22 -0300

    [UPDATE] Adicionando CSS local

--- app/info.php
+++ app/info.php
@@ -7,33 +7,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/my-style.css">
 
     <title>Ministério da Defesa!</title>
-    <style>
-    body {
-        padding: 20px;
-        0 px;
-        font-family: 'Open Sans', sans-serif;
-    }
-
-    .logo {
-        margin-top: 20px;
-        margin-left: 46%;
-    }
-    </style>
 </head>
 
 <body class="container">
     <div class="card text-center">
-        <img class="logo img-fluid" width="100" src="assets/img/logo.png" alt="Ministério da Defesa">
+        <img class="logophp img-fluid" width="100" src="assets/img/logo.png" alt="Ministério da Defesa">
         <div class="card-body">
             <h5 class="card-title">Gerência de Sistemas e Inovação - GESIS</h5>
             <p class="card-text">Imagem Base utilizada pelo Ministério da Defesa.</p>
             <a href="https://github.com/leonardofaria00/httpd-base" target="_blank" class="btn btn-primary">Ir para o
             repositório</a>
-            <a href="index.html" class="btn btn-default">Voltar</a>
+            <a href="index.html" class="btn btn-success">Voltar</a>
         </div>
     </div>
     <section class="card">
@@ -49,10 +37,3 @@
 </body>
 
 </html>
-
-<?php
-
-    // Show all information, defaults to INFO_ALL
-    phpinfo();
-
-?>
