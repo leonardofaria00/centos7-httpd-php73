453f543a7c7a4d7108fd4ffef0614e59c67d7982
leonardofaria00 <leonardofaria00@gmail.com>
2020-05-11 13:17:50 -0300

    [UPDATE] Adicionando Logo do MD

--- app/info.php
+++ app/info.php
@@ -11,24 +11,36 @@
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>Ministério da Defesa!</title>
+    <style>
+    body {
+        padding: 20px;
+        0 px;
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    .logo {
+        margin-top: 20px;
+        margin-left: 46%;
+    }
+    </style>
 </head>
 
 <body class="container">
     <div class="card text-center">
-        <div class="card-header">
-            Featured
-        </div>
+        <img class="logo img-fluid" width="100" src="assets/img/logo.png" alt="Ministério da Defesa">
         <div class="card-body">
             <h5 class="card-title">Gerência de Sistemas e Inovação - GESIS</h5>
             <p class="card-text">Imagem Base utilizada pelo Ministério da Defesa.</p>
-            <a href="https://github.com/leonardofaria00/httpd-base" target="_blank" class="btn btn-primary">Go to repository</a>
-        </div>        
+            <a href="https://github.com/leonardofaria00/httpd-base" target="_blank" class="btn btn-primary">Ir para o
+            repositório</a>
+            <a href="index.html" class="btn btn-default">Voltar</a>
+        </div>
     </div>
     <section class="card">
         <article class="text-center">
-       <span class="font-weight-bold">Host:</span> <?= $_SERVER['HTTP_HOST']?>
-        <br>
-        <span class="font-weight-bold">Apache Version:</span> <?= $_SERVER['SERVER_SOFTWARE']?>
+            <span class="font-weight-bold">Host:</span> <?= $_SERVER['HTTP_HOST']?>
+            <br>
+            <span class="font-weight-bold">Apache Version:</span> <?= $_SERVER['SERVER_SOFTWARE']?>
         </article>
         <div class="card-footer text-muted text-center">
             <span class="font-weight-bold">Date:</span> <?= date('m/d/Y h:i', time());?>
